385eccfb7719b3146acc87015c8a9efdf53063d1
kalebfukuda <kalebfukudajp@gmail.com>
2025-09-17 12:20:58 +0900

    Added svg animation for goliath

--- src/pages/Goliath.jsx
+++ src/pages/Goliath.jsx
@@ -1,6 +1,8 @@
 import Password from "../components/Password";
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import WinAudio from "../assets/audio/win.mp3";
+import Golias from "../assets/svg/Golias.svg";
+import GoliasDefeat from "../assets/svg/GoliasDefeat.svg";
 
 export default function Goliath() {
   const [isSuccess, setIsSuccess] = useState(false);
@@ -22,6 +24,7 @@ export default function Goliath() {
               "linear-gradient(0deg,rgba(226, 140, 19, 1) 0%, rgba(255, 247, 160, 1) 100%)",
           }}
         >
+          <object data={Golias} type=""></object>
           <Password
             id="goliath"
             pass="Goliath"
@@ -29,15 +32,19 @@ export default function Goliath() {
             inputClass="p-2 bg-white text-black rounded mr-2"
             onSuccess={handleSuccess}
           />
-
-          <img
-            src="src\assets\images\Golias.png"
-            className="w-full h-128 relative"
-            alt=""
-          />
         </div>
       )}
-      {isSuccess && <h1>Congrats!</h1>}
+      {isSuccess && (
+        <div
+          className="w-screen flex flex-col items-center justify-center h-screen"
+          style={{
+            background:
+              "linear-gradient(0deg,rgba(226, 140, 19, 1) 0%, rgba(255, 247, 160, 1) 100%)",
+          }}
+        >
+          <object data={GoliasDefeat} type=""></object>
+        </div>
+      )}
       <audio ref={winRef}>
         <source src={WinAudio} type="audio/mpeg" />
       </audio>
