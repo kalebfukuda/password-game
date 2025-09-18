7ada1d022d1b8757f631555738c4bb4954182722
KALEB DAN FUKUDA <kalebfukudajp@gmail.com>
2025-09-18 19:04:50 +0900

    Update Goliath.jsx
    More changes

--- src/pages/Goliath.jsx
+++ src/pages/Goliath.jsx
@@ -16,9 +16,7 @@ export default function Goliath() {
   };
   return ( 
     <div>
-      <h1>HELLO</h1>
       {!isSuccess && (
-        
         <div
           className="w-screen flex flex-col items-center justify-center h-screen"
           style={{
@@ -26,7 +24,6 @@ export default function Goliath() {
               "linear-gradient(0deg,rgba(226, 140, 19, 1) 0%, rgba(255, 247, 160, 1) 100%)",
           }}
         >
-          <h1>HELLO!!!</h1>
           <object data={Golias} type=""></object>
           <Password
             id="goliath"
